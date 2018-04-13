7a96cc566815a61d9bccf8945d48dde1f0eee00e
Matthew DesEnfants <matt.desenfants@gmail.com>
2018-04-13 15:25:07 -0700

    input boxes and some style changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,28 +6,25 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">CENSAI</h1>
+          <h1 className="App-title">CENS|AI</h1>
         </header>
         <div className="App-steps">
           <div className="Step Complete">
-            <h2>Submit your track</h2>
+            <h2>Submit your track<small>Provide a sharing link to Dropbox, OneDrive, or Google Drive.</small></h2>
+            <input type="url" className="" placeholder="https://onedropdrivebox.cloud/kd903209c90mwdcmiomscm" />
           </div>
           <div className="Step Active">
-            <h2>Submit Track</h2>
-            <p>Waveform goes here</p>
+            <h2>Preview track<small>Verify that the waveform looks right to you.</small></h2>
           </div>
           <div className="Step Waiting">
-            <h2>Preview</h2>
+            <h2>Confirm email<small>Make sure we can reach you when procesing is done.</small></h2>
+            <input type="email" className="" placeholder="george@jungle.ook" />
           </div>
           <div className="Step Waiting">
-            <h2>Confirm email</h2>
-            <p>Make sure we can reach you when procesing is done.</p>
+            <h2>Billing<small>Now for the hard part.</small></h2>
           </div>
           <div className="Step Waiting">
-            <h2>Billing</h2>
-          </div>
-          <div className="Step Waiting">
-            <h2>Download</h2>
+            <h2>Done<small>Download your marker track here.</small></h2>
           </div>
         </div>
       </div>
