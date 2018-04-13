90cab5f7c228f3912da037601adf3a41cb63245d
Matthew DesEnfants <matt.desenfants@gmail.com>
2018-04-13 16:19:00 -0700

    good ui

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,34 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">CENS|AI</h1>
+          <h1 className="App-title"><span className="light">CENS|</span>AI</h1>
         </header>
         <div className="App-steps">
+          <div className="Step Info">
+            <p>
+              <span className="light">CENS|</span>AI will comb through your podcast looking for
+              verbal fillers, profanities,
+              awkward pauses, and editing words.
+              </p>
+            <p>
+              Once it's done processing, you will get audio track that marks each point
+              of interest with a loud, visible click.
+            </p>
+          </div>
           <div className="Step Complete">
-            <h2>Submit your track<small>Provide a sharing link to Dropbox, OneDrive, or Google Drive.</small></h2>
-            <input type="url" className="" placeholder="https://onedropdrivebox.cloud/kd903209c90mwdcmiomscm" />
+            <h2>Submit your track<small>Paste a share link from Drive, Dropbox, or OneDrive.</small></h2>
+            <input
+              type="url"
+              className=""
+              placeholder="https://onedropdrivebox.cloud/5up3rc4l1fr461l1571c3xp14l1d0c10u5"
+            />
           </div>
           <div className="Step Active">
-            <h2>Preview track<small>Verify that the waveform looks right to you.</small></h2>
-          </div>
-          <div className="Step Waiting">
             <h2>Confirm email<small>Make sure we can reach you when procesing is done.</small></h2>
             <input type="email" className="" placeholder="george@jungle.ook" />
+          </div>
+          <div className="Step Waiting">
+            <h2>Preview track<small>Verify that the waveform looks right to you.</small></h2>
           </div>
           <div className="Step Waiting">
             <h2>Billing<small>Now for the hard part.</small></h2>
