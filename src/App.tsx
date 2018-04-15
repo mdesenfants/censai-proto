ed3b6d32e861c0c604b36778a5ee100bd03481c0
Matthew DesEnfants <matt.desenfants@gmail.com>
2018-04-14 17:26:38 -0700

    simple form validation

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,34 @@ let logo = (
   </span>
 );
 
-class App extends React.Component {
+interface AppState {
+  validUrl: boolean;
+  validEmail: boolean;
+}
+
+function inputIsValid(id: string): boolean {
+  let el = document.getElementById(id) as HTMLInputElement;
+  return el.checkValidity() && el.value.trim() !== '';
+}
+
+class App extends React.Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { validUrl: false, validEmail: false };
+  }
+
+  checkValidUrl = () => {
+    this.setState({
+      validUrl: inputIsValid('url')
+    });
+  }
+
+  checkValidEmail = () => {
+    this.setState({
+      validEmail: inputIsValid('email')
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -29,18 +56,28 @@ class App extends React.Component {
             <h2>Submit your track<small>Paste a share link from Drive, Dropbox, or OneDrive.</small></h2>
             <div className="Input-block">
               <input
+                id="url"
                 type="url"
                 className=""
                 placeholder="https://onedropdrivebox.cloud/5up3rc4l1fr461l1571c3xp14l1d0c10u5"
+                pattern="https://.+/.+"
+                onChange={this.checkValidUrl}
               />
-              <input type="button" value="Go" />
+              <input type="button" value="Go" disabled={!this.state.validUrl} />
             </div>
           </div>
           <div className="Step Active">
             <h2>Confirm email<small>Make sure we can reach you when procesing is done.</small></h2>
             <div className="Input-block">
-              <input type="email" className="" placeholder="george@jungle.ook" />
-              <input type="button" value="Go" />
+              <input
+                id="email"
+                type="email"
+                className=""
+                placeholder="george@jungle.ook"
+                pattern=".+@.+\..+"
+                onChange={this.checkValidEmail}
+              />
+              <input type="button" value="Go" disabled={!this.state.validEmail} />
             </div>
           </div>
           <div className="Step Waiting">
